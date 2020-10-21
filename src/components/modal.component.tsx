96605b2b8c9d33f97ceaf3e8f70b8b5847dae507
Ilya Konstantinov <ilykonst95@gmail.com>
2020-10-21 14:56:17 +0400

    feat(modal)

---
+++ src/components/modal.component.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./modal.module.scss";
+
+interface IProps {
+  onClose: () => void;
+}
+
+export const Modal: React.FC<IProps> = (props) => {
+  return (
+    <React.Fragment>
+      <div className={styles.overlay} onClick={props.onClose}>
+        <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
+          {props.children}
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
