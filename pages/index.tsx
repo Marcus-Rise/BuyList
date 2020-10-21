2894a806635f7ec576decf37fb319d5759b0cce1
Ilya Konstantinov <ilykonst95@gmail.com>
2020-10-21 12:20:57 +0400

    refactor(pages/index.tsx)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { ProductList } from "../src/product-list/product-list.component";
 import { IProductList } from "../src/product-list/product-list.interface";
 import { IProduct } from "../src/product/product.interface";
 import { useInject } from "../src/ioc/use-inject.decorator";
 import { IProductListService, PRODUCT_LIST_SERVICE_PROVIDER } from "../src/product-list/product-list.service-interface";
+import { Button, Colors } from "../src/components/button.component";
+import { BsPlus } from "react-icons/bs";
+import { ProductListItem } from "../src/product-list/product-list-item.component";
+import { BudgetCalculateForm } from "../src/budget/budget-calculate-form.component";
 
 const Home: React.FC = () => {
   const service = useInject<IProductListService>(PRODUCT_LIST_SERVICE_PROVIDER);
@@ -24,12 +27,48 @@ const Home: React.FC = () => {
     }
   };
 
+  const onItemToggle = (item: IProduct): void => {
+    if (list) {
+      const newArr: IProduct[] = [...list.items];
+      const index = list.items.findIndex((i) => i.uuid === item.uuid);
+
+      newArr[index].active = !item.active;
+
+      onItemsChange(newArr);
+    }
+  };
+
+  const onCalculationRequire = (val: number) => {
+    console.debug("budget: ", val);
+  };
+
   return (
-    <div className="container pt-3">
-      <div className="row">
-        <div className="col">{list && <ProductList {...list} onItemsChange={onItemsChange} />}</div>
+    list && (
+      <div className="container pt-3">
+        <div className="row">
+          <div className="col-12 d-flex align-items-center justify-content-center">
+            <h2>{list.title}</h2>
+            <Button className="ml-3 p-2" rounded color={Colors.accent} onClick={() => undefined}>
+              <BsPlus size={"2rem"} />
+            </Button>
+          </div>
+          <div className="col-12 py-4">
+            <BudgetCalculateForm onSubmit={onCalculationRequire} />
+          </div>
+          <div className="col">
+            <div className="row">
+              <div className="card-body">
+                <ul className="list-group">
+                  {list.items.map((i, index) => (
+                    <ProductListItem key={i.uuid} {...i} index={index} onToggle={() => onItemToggle(i)} />
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
