9c6a38883b9a18861297da8761aaf2b74d4b6bdf
Ilya Konstantinov <ilykonst95@gmail.com>
2020-10-21 16:15:07 +0400

    feat(product-list.service-interface.ts)

--- src/product-list/product-list.service-interface.ts
+++ src/product-list/product-list.service-interface.ts
@@ -1,7 +1,10 @@
 import { IProductList } from "./product-list.interface";
+import { IProduct } from "../product/product.interface";
 
 export const PRODUCT_LIST_SERVICE_PROVIDER = Symbol("IProductListService");
 
 export interface IProductListService {
   getLatest(): Promise<IProductList>;
+
+  save(list: IProductList, item: Partial<IProduct>): Promise<IProductList>;
 }
--- src/product-list/product-list.service.ts
+++ src/product-list/product-list.service.ts
@@ -13,6 +13,38 @@ export class ProductListService implements IProductListService {
     faker.locale = "ru";
   }
 
+  async save(list: IProductList, item: Partial<IProduct>): Promise<IProductList> {
+    let newArr: IProduct[];
+
+    if (item.uuid) {
+      newArr = [...list.items];
+      const index = list.items.findIndex((i) => i.uuid === item.uuid);
+
+      newArr[index] = {
+        uuid: item.uuid,
+        active: !!item.active,
+        title: item.title ?? "",
+        price: item.price ?? 0,
+        priority: item.priority ?? ProductPriorityEnum.middle,
+      };
+    } else {
+      const newItem: IProduct = {
+        uuid: faker.random.uuid(),
+        active: !!item.active,
+        title: item.title ?? "",
+        price: item.price ?? 0,
+        priority: item.priority ?? ProductPriorityEnum.middle,
+      };
+
+      newArr = [newItem, ...list.items];
+    }
+
+    return {
+      ...list,
+      items: newArr,
+    };
+  }
+
   async getLatest(): Promise<IProductList> {
     if (!this.list) {
       this.list = this.generateList(10);
