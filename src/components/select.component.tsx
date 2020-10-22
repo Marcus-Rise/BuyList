5ac4246c3ff57e7017ba76cde439c20066ee7658
Ilya Konstantinov <ilykonst95@gmail.com>
2020-10-22 11:56:47 +0400

    feat(select)

---
+++ src/components/select.component.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./select.module.scss";
+
+export interface SelectOption<T> {
+  title: string;
+  val: T;
+}
+
+interface IProps<T> {
+  items: SelectOption<T>[];
+  label: string;
+  val?: T;
+  onChange: (val: T) => void;
+}
+
+export const Select: React.FC<IProps<string>> = (props) => {
+  return (
+    <div>
+      <label className={styles.label} htmlFor={props.label}>
+        {props.label}
+      </label>
+      <div className={styles.select}>
+        <select id={props.label} value={props.val} onChange={(e) => props.onChange(e.target.value)}>
+          <option disabled>{props.label}</option>
+          {props.items.map((i) => (
+            <option key={i.title} value={i.val}>
+              {i.title}
+            </option>
+          ))}
+        </select>
+        <span className="focus" />
+      </div>
+    </div>
+  );
+};
