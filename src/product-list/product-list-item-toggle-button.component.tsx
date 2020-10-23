e2c080284959b00c61a163a1668db968b788680f
Ilya Konstantinov <ilykonst95@gmail.com>
2020-10-23 12:32:30 +0400

    feat(product-list-item-toggle-button)

---
+++ src/product-list/product-list-item-toggle-button.component.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./product-list-item-toggle-button.module.scss";
+import { FaCheck } from "react-icons/fa";
+import { Button } from "../components/button.component";
+
+interface IProps {
+  active: boolean;
+  onClick: () => void;
+}
+
+export const ProductListItemToggleButton: React.FC<IProps> = (props) => {
+  return (
+    <Button
+      className={`${styles.toggleButton}`}
+      styles={{ padding: "10px" }}
+      rounded
+      onClick={(e) => {
+        e.stopPropagation();
+        props.onClick();
+      }}
+    >
+      <FaCheck style={props.active ? { color: "white" } : {}} />
+    </Button>
+  );
+};
