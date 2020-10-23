7cab8d01f505fe81a6b16d0528d0b885b234b4b3
Ilya Konstantinov <ilykonst95@gmail.com>
2020-10-23 12:11:43 +0400

    feat(container.ts)

--- src/ioc/container.ts
+++ src/ioc/container.ts
@@ -1,10 +1,13 @@
 import "reflect-metadata";
 import { Container } from "inversify";
 import { IProductListService, PRODUCT_LIST_SERVICE_PROVIDER } from "../product-list/product-list.service-interface";
-import { ProductListService } from "../product-list/product-list.service";
+import { ProductListServiceMock } from "../product-list/product-list.service-mock";
+import { BUDGET_SERVICE_PROVIDER, IBudgetService } from "../budget/budget.service-interface";
+import { BudgetServiceMock } from "../budget/budget.service-mock";
 
 const container = new Container();
 
-container.bind<IProductListService>(PRODUCT_LIST_SERVICE_PROVIDER).to(ProductListService).inSingletonScope();
+container.bind<IProductListService>(PRODUCT_LIST_SERVICE_PROVIDER).to(ProductListServiceMock).inSingletonScope();
+container.bind<IBudgetService>(BUDGET_SERVICE_PROVIDER).to(BudgetServiceMock).inSingletonScope();
 
 export { container };
