23a120a227748bfc63c053cb4e3a76106c1f8d7e
Ilya Konstantinov <ilykonst95@gmail.com>
2020-10-25 14:57:37 +0400

    feat(container.ts): use indexed db

--- src/ioc/container.ts
+++ src/ioc/container.ts
@@ -8,13 +8,13 @@ import { BudgetService } from "../budget/budget.service";
 import type { IProductListRepository } from "../product-list/product-list.repository-interface";
 import { PRODUCT_LIST_REPOSITORY_PROVIDER } from "../product-list/product-list.repository-interface";
 import { ProductListService } from "../product-list/product-list.service";
-import { ProductListInMemoryRepository } from "../product-list/product-list-in-memory.repository";
+import { ProductListIndexedDbRepository } from "../product-list/product-list-indexed-db.repository";
 
 const container = new Container();
 
 container
   .bind<IProductListRepository>(PRODUCT_LIST_REPOSITORY_PROVIDER)
-  .to(ProductListInMemoryRepository)
+  .to(ProductListIndexedDbRepository)
   .inSingletonScope();
 container.bind<IProductListService>(PRODUCT_LIST_SERVICE_PROVIDER).to(ProductListService).inSingletonScope();
 container.bind<IBudgetService>(BUDGET_SERVICE_PROVIDER).to(BudgetService).inSingletonScope();
