5c292d2a1e9a7f3653f1358802071df8cd6ce13b
Ilya Konstantinov <ilykonst95@gmail.com>
2020-10-25 18:53:25 +0400

    refactor(header.component.tsx)

--- src/components/header.component.tsx
+++ src/components/header.component.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./header.module.scss";
 import humburger from "../assets/icons/humburger.svg";
-import logo from "../assets/icons/logo_icon.svg";
+import logo from "../assets/icons/logo.svg";
 
 export const Header: React.FC = () => {
   const title = "BuyList";
@@ -12,7 +12,7 @@ export const Header: React.FC = () => {
         <div className="row justify-content-between align-items-center">
           <div className="col">
             <a className={`${styles.link} d-flex align-items-center justify-content-start`} href={"/"}>
-              <img className={`${styles.logo} mr-2`} src={logo} alt={title} />
+              <img className={` mr-2`} src={logo} alt={title} />
               <h1 className={styles.title}>{title}</h1>
             </a>
           </div>
