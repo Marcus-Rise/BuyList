e78e82cf9a1b8e3c754d6a97455fe6dac8c38330
Ilya Konstantinov <ilykonst95@gmail.com>
2020-10-25 15:57:21 +0400

    refactor(price-stringify.function.ts)

--- src/components/price-stringify.function.ts
+++ src/components/price-stringify.function.ts
@@ -1,17 +1,7 @@
 const PriceStringify = (val: number): string => {
-  let buf = String(val);
-  let text = "";
-
-  const DIVIDER = 3;
-
-  while (buf.length > DIVIDER) {
-    const subStr = buf.substr(buf.length - DIVIDER);
-    text = ` ${subStr}` + text;
-
-    buf = buf.substr(0, buf.length - DIVIDER);
-  }
-
-  return buf + text;
+  return String(val)
+    .replace(/\D/g, "")
+    .replace(/\B(?=(\d{3})+(?!\d))/g, " ");
 };
 
 export { PriceStringify };
