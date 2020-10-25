bfcf95db4a5e06bb277a1a6538f233a50a82212d
Ilya Konstantinov <ilykonst95@gmail.com>
2020-10-25 18:53:16 +0400

    feat(button-add.component.tsx)

---
+++ src/components/button-add.component.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Button, ButtonColors } from "./button.component";
+import PlusIcon from "../assets/icons/plus_icon.svg";
+import styles from "./button-add.module.scss";
+
+interface IProps {
+  onClick: () => void;
+  className?: string;
+}
+
+const ButtonAdd: React.FC<IProps> = (props) => {
+  return (
+    <Button
+      className={`${props.className} ${styles.button}`}
+      rounded
+      color={ButtonColors.accent}
+      onClick={props.onClick}
+    >
+      <img alt={"add"} src={PlusIcon} />
+    </Button>
+  );
+};
+
+export { ButtonAdd };
