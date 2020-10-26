f2e45fc72612fb1c855624259998f801986cb09c
Ilya Konstantinov <ilykonst95@gmail.com>
2020-10-26 10:03:50 +0400

    fix(header.component.tsx)

--- src/components/header.component.tsx
+++ src/components/header.component.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./header.module.scss";
 import humburger from "../assets/icons/humburger.svg";
 import logo from "../assets/icons/logo.svg";
+import Link from "next/link";
 
 export const Header: React.FC = () => {
   const title = "BuyList";
@@ -11,10 +12,12 @@ export const Header: React.FC = () => {
       <div className="container">
         <div className="row justify-content-between align-items-center">
           <div className="col">
-            <a className={`${styles.link} d-flex align-items-center justify-content-start`} href={"/"}>
-              <img className={`${styles.menu} mr-2`} src={logo} alt={title} />
-              <h1 className={styles.title}>{title}</h1>
-            </a>
+            <Link href={"/"}>
+              <div className={`${styles.link} d-flex align-items-center justify-content-start`}>
+                <img className={`${styles.menu} mr-2`} src={logo} alt={title} />
+                <h1 className={styles.title}>{title}</h1>
+              </div>
+            </Link>
           </div>
           <div className="col-auto">
             <img className={styles.menu} src={humburger} alt={"menu"} />
