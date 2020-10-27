8c23d98264e3cfbbe80ebd0b8e7810f9f5be3612
Ilya Konstantinov <ilykonst95@gmail.com>
2020-10-27 11:59:48 +0400

    refactor(modal.component.tsx)

--- src/components/modal.component.tsx
+++ src/components/modal.component.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect } from "react";
 import styles from "./modal.module.scss";
 
 interface IProps {
@@ -7,17 +7,21 @@ interface IProps {
 
 const Modal: React.FC<IProps> = (props) => {
   useEffect(() => {
-    document.getElementsByTagName("body")[0].classList.add(styles.body);
+    const body = document.getElementsByTagName("body")[0];
+
+    body.classList.add(styles.body);
 
     return () => {
-      document.getElementsByTagName("body")[0].classList.remove(styles.body);
+      body.classList.remove(styles.body);
     };
   }, []);
+
+  const onModalClick = useCallback((e: React.MouseEvent) => e.stopPropagation(), []);
 
   return (
     <React.Fragment>
       <div className={`${styles.overlay}`} onClick={props.onClose}>
-        <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
+        <div className={styles.modal} onClick={onModalClick}>
           {props.children}
         </div>
       </div>
