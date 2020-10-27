73ca10e1af0e9605a9a51d6cc1d9c0ec9c9d22df
Ilya Konstantinov <ilykonst95@gmail.com>
2020-10-27 16:57:46 +0400

    refactor(product-list-item-toggle-button.component.tsx)

--- src/product-list/product-list-item-toggle-button.component.tsx
+++ src/product-list/product-list-item-toggle-button.component.tsx
@@ -9,15 +9,17 @@ interface IProps {
 }
 
 const ProductListItemToggleButton: React.FC<IProps> = (props) => {
+  const onClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    props.onClick();
+  };
+
   return (
     <Button
       className={`${styles.toggleButton} ${props.active ? styles.inActive : styles.active}`}
       styles={{ padding: ".5rem" }}
       rounded
-      onClick={(e) => {
-        e.stopPropagation();
-        props.onClick();
-      }}
+      onClick={onClick}
     >
       {!props.active && <img alt={"in_active"} className={styles.img} src={InActiveIcon} />}
     </Button>
