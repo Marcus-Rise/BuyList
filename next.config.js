0d17807935a669e15189e69e767e0dddf5479b27
Ilya Konstantinov <ilykonst95@gmail.com>
2020-11-06 23:41:56 +0400

    build(next.config.js): add offline config & fix preload

--- next.config.js
+++ next.config.js
@@ -2,18 +2,22 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.NODE_ENV === "production",
 });
+const withOffline = require("next-offline");
 
-module.exports = withBundleAnalyzer({
-  images: {
-    deviceSizes: [320, 420, 768, 1024, 1200],
-  },
-  reactStrictMode: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack", "url-loader"],
-    });
+module.exports = withOffline(
+  withBundleAnalyzer({
+    crossOrigin: "anonymous",
+    images: {
+      deviceSizes: [320, 420, 768, 1024, 1200],
+    },
+    reactStrictMode: true,
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ["@svgr/webpack", "url-loader"],
+      });
 
-    return config;
-  },
-});
+      return config;
+    },
+  }),
+);
