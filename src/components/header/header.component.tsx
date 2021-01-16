7872bc112be69490e55bc159504a838b9150b033
Ilya Konstantinov <ilykonst95@gmail.com>
2021-01-16 12:59:43 +0400

    refactor(header.component.tsx)

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./header.module.scss";
+import gridStyles from "../../styles/grid.module.scss";
 import humburger from "../../assets/icons/humburger.svg";
 import logo from "../../assets/icons/logo.svg";
 import Link from "next/link";
@@ -8,22 +9,14 @@ import classNames from "classnames";
 const Header: React.FC<{ appName: string }> = (props) => {
   return (
     <header className={classNames(styles.root)}>
-      <div className="container">
-        <div className={classNames("row", "justify-content-between", "align-items-center")}>
-          <div className="col">
-            <Link href={"/"}>
-              <a className={styles.link}>
-                <div className={classNames("d-flex", "align-items-center", "justify-content-start")}>
-                  <img className={classNames(styles.menu, "mr-2")} src={logo} alt={props.appName} />
-                  <h1 className={styles.title}>{props.appName}</h1>
-                </div>
-              </a>
-            </Link>
-          </div>
-          <div className="col-auto">
-            <img className={styles.menu} src={humburger} alt={"menu"} />
-          </div>
-        </div>
+      <div className={classNames(gridStyles.container, styles.content)}>
+        <Link href={"/"}>
+          <a className={styles.link}>
+            <img className={classNames(styles.menu)} src={logo} alt={props.appName} />
+            <h1 className={styles.title}>{props.appName}</h1>
+          </a>
+        </Link>
+        <img className={styles.menu} src={humburger} alt={"menu"} />
       </div>
     </header>
   );
