4f44befb157c15f39d17ad79809f14006f2259ce
Ilya Konstantinov <ilykonst95@gmail.com>
2021-01-16 17:54:07 +0400

    fix([...nextauth].ts)

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,15 +1,18 @@
+import type { InitOptions } from "next-auth";
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 import type { NextApiHandler } from "next";
 
-const options = {
+const options: InitOptions = {
   providers: [
     Providers.Google({
       clientId: String(process.env.GOOGLE_CLIENT_ID),
       clientSecret: String(process.env.GOOGLE_CLIENT_SECRET),
+      authorizationUrl:
+        "https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code",
     }),
   ],
-
+  debug: process.env.NODE_ENV === "development",
   // A database is optional, but required to persist accounts in a database
   // database: process.env.DATABASE_URL,
 };
