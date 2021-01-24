1657b2643c2847aa87b232caee54ed5b194e0529
Ilya Konstantinov <ilykonst95@gmail.com>
2021-01-24 15:10:48 +0400

    feat(api test url): get appdata file list

--- pages/api/test/index.ts
+++ pages/api/test/index.ts
@@ -13,26 +13,20 @@ const TestHandler: NextApiHandler = async (req, res) => {
   const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
   const accessToken = session?.accessToken;
   const refreshToken = session?.refreshToken;
-  const apiKey = process.env.GOOGLE_API_KEY;
-  const projectId = process.env.GOOGLE_PROJECT_ID;
-  const scopes = ["https://www.googleapis.com/auth/drive.appdata"];
 
   const auth = new google.auth.OAuth2({
     clientId,
     clientSecret,
   });
-  auth.projectId = projectId;
-  auth.apiKey = apiKey;
   auth.setCredentials({
     access_token: accessToken,
     refresh_token: refreshToken,
-    scope: scopes.join(" "),
   });
 
   const drive = google.drive({ auth, version: "v3" });
 
   await drive.files
-    .list()
+    .list({ spaces: "appDataFolder" })
     .then((data) => {
       console.debug(data.data);
       res.json(data.data);
