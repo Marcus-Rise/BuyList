17dd269d7b792147804d226069145c861e257a5a
Ilya Konstantinov <ilykonst95@gmail.com>
2021-01-27 13:37:29 +0400

    feat(auth): refresh token

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,10 +2,61 @@ import type { InitOptions, User } from "next-auth";
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 import type { NextApiHandler } from "next";
-import type { GenericObject, SessionBase } from "next-auth/_utils";
+import type { GenericObject } from "next-auth/_utils";
 import type { IGoogleConfig } from "../../../src/server/google";
 import { GOOGLE_CONFIG } from "../../../src/server/google";
 import { inject } from "../../../src/server/ioc";
+import type { AuthPayload } from "../../../src/server/auth/auth-payload.interface";
+
+/**
+ * Takes a token, and returns a new token with updated
+ * `accessToken` and `accessTokenExpires`. If an error occurs,
+ * returns the old token and an error property
+ */
+const refreshAccessToken = async (
+  payload: AuthPayload,
+  clientId: string,
+  clientSecret: string,
+): Promise<AuthPayload> => {
+  try {
+    const url = new URL("https://oauth2.googleapis.com/token");
+    url.searchParams.set("client_id", clientId);
+    url.searchParams.set("client_secret", clientSecret);
+    url.searchParams.set("grant_type", "refresh_token");
+    url.searchParams.set("refresh_token", payload.refreshToken);
+
+    const response = await fetch(url.toString(), {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      method: "POST",
+    });
+
+    const refreshToken = await response.json();
+
+    if (!response.ok) {
+      throw refreshToken;
+    }
+
+    // Give a 10 sec buffer
+    const now = new Date();
+    const accessTokenExpires = now.setSeconds(now.getSeconds() + refreshToken.expires_in - 10);
+
+    return {
+      ...payload,
+      accessToken: refreshToken.access_token,
+      accessTokenExpires,
+      refreshToken: refreshToken.refresh_token,
+    };
+  } catch (error) {
+    console.error(error);
+
+    return {
+      ...payload,
+      error: "RefreshAccessTokenError",
+    };
+  }
+};
 
 const AuthHandler: NextApiHandler = (req, res, googleConfig = inject<IGoogleConfig>(GOOGLE_CONFIG)) => {
   const scopes = [
@@ -35,21 +86,35 @@ const AuthHandler: NextApiHandler = (req, res, googleConfig = inject<IGoogleConf
     },
     debug: process.env.NODE_ENV === "development",
     callbacks: {
-      async jwt(token: GenericObject, user: User, account: GenericObject): Promise<GenericObject> {
-        if (account) {
-          token.accessToken = account?.accessToken;
-          token.refreshToken = account?.refreshToken;
+      async jwt(payload: AuthPayload, user: User, account: GenericObject): Promise<AuthPayload> {
+        let res: AuthPayload;
+
+        const now = Date.now();
+
+        // Signing in
+        if (account && user) {
+          const accessToken = account.accessToken;
+          const refreshToken = account.refreshToken;
+
+          res = {
+            accessToken,
+            accessTokenExpires: account.accessTokenExpires,
+            refreshToken,
+            user,
+          };
+        } else if (payload.accessTokenExpires === null || now < payload.accessTokenExpires) {
+          // Subsequent use of JWT, the user has been logged in before
+          // access token has not expired yet
+          res = payload;
+        } else {
+          // access token has expired, try to update it
+          res = await refreshAccessToken(payload, googleConfig.clientId, googleConfig.clientSecret);
         }
 
-        return token;
+        return res;
       },
-      async session(session: SessionBase, user: any): Promise<GenericObject> {
-        session.accessToken = user.accessToken;
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        session.refreshToken = user.refreshToken;
-
-        return session;
+      async session(_, payload: GenericObject): Promise<GenericObject> {
+        return payload;
       },
     },
   };
