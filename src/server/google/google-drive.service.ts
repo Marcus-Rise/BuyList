5ce61b2e12956a870203e19f3bb3f1289993e30e
Ilya Konstantinov <ilykonst95@gmail.com>
2021-01-31 13:29:35 +0400

    refactor(google drive)

--- src/server/google/google-drive.service.ts
+++ src/server/google/google-drive.service.ts
@@ -27,7 +27,9 @@ class GoogleDriveService implements IGoogleDriveService {
     auth.forceRefreshOnFailure = true;
     auth.setCredentials({
       access_token: this.authConfig.accessToken,
+      refresh_token: this.authConfig.refreshToken,
     });
+    auth.forceRefreshOnFailure = true;
 
     return google.drive({ auth, version: "v3" });
   }
@@ -38,24 +40,95 @@ class GoogleDriveService implements IGoogleDriveService {
     return { code: error?.code ?? 500, text: String(error ?? e) };
   }
 
-  async createFile(name: string, mimeType: string, data: string): Promise<IFileInfo | IError> {
-    let res: IFileInfo | IError = { code: 500, text: "Unexpected error" };
+  isError(obj: unknown): obj is IError {
+    return typeof obj === "object" && obj !== null && "code" in obj && "text" in obj;
+  }
+
+  async writeFile(name: string, mimeType: string, data: string, update?: boolean): Promise<IFileInfo | IError> {
+    let res: IFileInfo | IError = { code: 500, text: "Unexpected error on writing file" };
+
+    if (update) {
+      const list = await this.getFileList();
+
+      if (!this.isError(list)) {
+        const fileId = list.find((i) => i.name === name)?.id;
+
+        if (fileId) {
+          res = await this.updateFile(fileId, data);
+        } else {
+          res = await this.createFile(name, mimeType, data);
+        }
+      }
+    } else {
+      res = await this.createFile(name, mimeType, data);
+    }
+
+    return res;
+  }
+
+  async getFileList(): Promise<IFileInfo[] | IError> {
+    let res: IFileInfo[] | IError = [];
 
     await this.drive.files
-      .create(
-        {
-          requestBody: {
-            name,
-            //todo return
-            /*parents: ["appDataFolder"]*/
-          },
-          media: { mimeType, body: data },
-        },
-        {},
-      )
+      .list({
+        // todo return
+        // spaces: "appDataFolder"
+      })
       .then((data) => {
-        console.debug(data.data);
+        const content = data.data;
+        res = content.files
+          ?.map<IFileInfo>((i) => ({ id: i.id ?? "", name: i.name ?? "" }))
+          ?.filter((i) => i.id && i.name) ?? {
+          code: 404,
+          text: "No files",
+        };
+      })
+      .catch((e) => {
+        res = GoogleDriveService.parseError(e);
+      });
 
+    return res;
+  }
+
+  async readFile(id: string): Promise<IError | string> {
+    const file = await this.drive.files.get({ fileId: id, alt: "media" }, { responseType: "stream" });
+
+    return new Promise<string>((resolve, reject) => {
+      const filePath = path.join(os.tmpdir(), uuid.v4());
+      const dest = fs.createWriteStream(filePath);
+
+      file.data
+        .on("end", () => {
+          fs.readFile(filePath, "utf8", (err, data) => {
+            if (err) {
+              const error: IError = { code: err.code ? Number(err.code) : 500, text: err.message };
+              reject(error);
+            } else {
+              resolve(data);
+            }
+          });
+        })
+        .on("error", (err) => {
+          const error: IError = { code: 500, text: err.message };
+          reject(error);
+        })
+        .pipe(dest);
+    });
+  }
+
+  async createFile(name: string, mimeType: string, data: string): Promise<IFileInfo | IError> {
+    let res: IFileInfo | IError = { code: 500, text: "Unexpected error on creating file" };
+
+    await this.drive.files
+      .create({
+        requestBody: {
+          name,
+          // todo return
+          // parents: ["appDataFolder"]
+        },
+        media: { mimeType, body: data },
+      })
+      .then((data) => {
         if (data.data.id && data.data.name) {
           res = {
             id: data.data.id,
@@ -75,77 +148,36 @@ class GoogleDriveService implements IGoogleDriveService {
     return res;
   }
 
-  async getFile(id: string): Promise<IFileInfo | IError> {
-    let res: IFileInfo | IError = { code: 500, text: "Unexpected error" };
+  async updateFile(id: string, data: string): Promise<IFileInfo | IError> {
+    let res: IFileInfo | IError = { code: 500, text: "Unexpected error on updating file" };
 
     await this.drive.files
-      .get({
+      .update({
         fileId: id,
-        fields: "*",
+        requestBody: {
+          // todo return
+          // parents: ["appDataFolder"]
+        },
+        media: { body: data },
       })
       .then((data) => {
-        const file = data.data;
-        console.debug(file);
+        if (data.data.id && data.data.name) {
+          res = {
+            id: data.data.id,
+            name: data.data.name,
+          };
+        } else {
+          res = {
+            code: 500,
+            text: "No file id",
+          };
+        }
       })
       .catch((e) => {
         res = GoogleDriveService.parseError(e);
       });
 
     return res;
-  }
-
-  async getFileList(): Promise<IFileInfo[] | IError> {
-    let res: IFileInfo[] | IError = [];
-
-    await this.drive.files
-      .list()
-      .then((data) => {
-        const content = data.data;
-        res = content.files
-          ?.map<IFileInfo>((i) => ({ id: i.id ?? "", name: i.name ?? "" }))
-          ?.filter((i) => i.id && i.name) ?? {
-          code: 500,
-          text: "No files",
-        };
-      })
-      .catch((e) => {
-        res = GoogleDriveService.parseError(e);
-      });
-
-    return res;
-  }
-
-  async readFile(id: string): Promise<IError | string> {
-    const file = await this.drive.files.get({ fileId: id, alt: "media" }, { responseType: "stream" });
-
-    return new Promise<string>((resolve, reject) => {
-      const filePath = path.join(os.tmpdir(), uuid.v4());
-      console.log(`writing to ${filePath}`);
-      const dest = fs.createWriteStream(filePath);
-
-      file.data
-        .on("end", () => {
-          console.log("Done downloading file.");
-
-          fs.readFile(filePath, "utf8", (err, data) => {
-            if (err) {
-              const error: IError = { code: err.code ? Number(err.code) : 500, text: err.message };
-              reject(error);
-            } else {
-              resolve(data);
-            }
-          });
-        })
-        .on("error", (err) => {
-          const error: IError = { code: 500, text: err.message };
-          reject(error);
-        })
-        .pipe(dest);
-    });
-  }
-
-  isError(obj: IFileInfo | IFileInfo[] | IError): obj is IError {
-    return "code" in obj && "text" in obj;
   }
 }
 
