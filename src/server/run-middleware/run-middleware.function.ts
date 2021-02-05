271c2a87060dd3dcaf0ed1073933481e9feaa941
Ilya Konstantinov <ilykonst95@gmail.com>
2021-02-05 10:48:48 +0400

    types(run-middleware.function.ts)

--- src/server/run-middleware/run-middleware.function.ts
+++ src/server/run-middleware/run-middleware.function.ts
@@ -1,4 +1,11 @@
-const runMiddleware = (req, res, fn) =>
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type CallBackResult = unknown;
+type CallBack = (result: CallBackResult) => void;
+
+type MiddleWare = (req: NextApiRequest, res: NextApiResponse, callback: CallBack) => void;
+
+const runMiddleware = (req: NextApiRequest, res: NextApiResponse, fn: MiddleWare): Promise<CallBackResult> =>
   new Promise((resolve, reject) => {
     fn(req, res, (result) => {
       if (result instanceof Error) {
