ecb3c23b721b21be43d57f13df3e51b9260f21c3
Ilya Konstantinov <ilykonst95@gmail.com>
2021-02-05 10:59:39 +0400

    feat(container.ts): client auth

--- src/ioc/container.ts
+++ src/ioc/container.ts
@@ -1,10 +1,11 @@
 import "reflect-metadata";
 import { Container } from "inversify";
 import { ProductListModule } from "../product-list";
+import { AuthModule } from "../auth";
 import { BudgetModule } from "../budget";
 
 const container = new Container();
 
-container.load(BudgetModule, ProductListModule);
+container.load(AuthModule, BudgetModule, ProductListModule);
 
 export { container };
