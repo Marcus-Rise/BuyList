7a30e00448c66541e3d77ba3b2a77edb9ebfb593
Ilya Konstantinov <ilykonst95@gmail.com>
2021-02-05 11:02:38 +0400

    feat(_app.tsx): share auth to services

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,34 @@
 import "../src/styles/globals.scss";
-import React from "react";
+import React, { useEffect } from "react";
 import { Header } from "../src/components";
 import "typeface-montserrat";
 import Head from "next/head";
 import { Footer } from "../src/components/footer.component";
 import type { AppComponent } from "next/dist/next-server/lib/router/router";
 import { Provider } from "next-auth/client";
+import { useInject } from "../src/ioc";
+import type { IAuthService } from "../src/auth";
+import { AUTH_SERVICE_PROVIDER } from "../src/auth";
 
-const MyApp: AppComponent = ({ Component, pageProps }) => (
-  <Provider session={pageProps.session}>
-    <Head>
-      <title>BuyList</title>
-    </Head>
-    <Header appName={"BuyList"} />
-    <main>
-      <Component {...pageProps} />
-    </main>
-    <Footer year={new Date().getFullYear()} author={"Ilya Konstantinov"} authorLink={"https://marcus-rise.dev"} />
-  </Provider>
-);
+const MyApp: AppComponent = ({ Component, pageProps }) => {
+  const authService = useInject<IAuthService>(AUTH_SERVICE_PROVIDER);
+
+  useEffect(() => {
+    authService.session = pageProps.session;
+  }, [authService, pageProps.session]);
+
+  return (
+    <Provider session={pageProps.session}>
+      <Head>
+        <title>BuyList</title>
+      </Head>
+      <Header appName={"BuyList"} />
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <Footer year={new Date().getFullYear()} author={"Ilya Konstantinov"} authorLink={"https://marcus-rise.dev"} />
+    </Provider>
+  );
+};
 
 export default MyApp;
