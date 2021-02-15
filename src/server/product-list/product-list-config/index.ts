80aef673450df157b2453413303bea20f5ffa209
Ilya Konstantinov <ilykonst95@gmail.com>
2021-02-15 15:44:09 +0400

    feat(server/product-list-config)

---
+++ src/server/product-list/product-list-config/index.ts
@@ -0,0 +1,3 @@
+export type { IProductListConfig } from "./product-list.config.interface";
+export { ProductListConfig } from "./product-list.config";
+export { PRODUCT_LIST_CONFIG_PROVIDER } from "./product-list.config.provider";
