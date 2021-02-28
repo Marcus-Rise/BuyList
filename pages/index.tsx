61282f4ea071536769aa75654e634df9c1e8782b
Marcus-Rise <ilykonst95@gmail.com>
2021-02-28 22:20:26 +0400

    refactor: homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,18 @@
 import type { FC } from "react";
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useMemo } from "react";
 import { useInject } from "../src/ioc";
 import type { IProductListService } from "../src/product-list";
 import { PRODUCT_LIST_SERVICE_PROVIDER } from "../src/product-list";
 import { observer } from "mobx-react";
 import Link from "next/link";
-import type { ProductListModel } from "../src/product-list/product-list.model";
 import { format } from "date-fns";
 
 const Home: React.FC<{
   service: IProductListService;
 }> = ({ service }) => {
-  const [listArray, setListArray] = useState<ProductListModel[]>([]);
-
-  useEffect(() => {
-    setListArray(service.listArray);
-  }, [service.listArray]);
-
   const items = useMemo(
     () =>
-      listArray.map((i) => (
+      service.listArray.map((i) => (
         <li key={i.id}>
           <Link href={`/product-list?id=${i.id}`}>
             <a>
@@ -28,7 +21,7 @@ const Home: React.FC<{
           </Link>
         </li>
       )),
-    [listArray],
+    [service.listArray],
   );
 
   return <ul>{items}</ul>;
