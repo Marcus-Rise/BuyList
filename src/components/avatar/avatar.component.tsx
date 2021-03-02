e5b7dc175d2a17bc7c341e19d49e4f201a14ee8f
Marcus-Rise <ilykonst95@gmail.com>
2021-03-02 19:55:22 +0400

    refactor(avatar.component.tsx)

--- src/components/avatar/avatar.component.tsx
+++ src/components/avatar/avatar.component.tsx
@@ -1,9 +1,22 @@
-import type { FC } from "react";
+import type { FC, ImgHTMLAttributes } from "react";
 import React from "react";
 import styles from "./avatar.module.scss";
+import classNames from "classnames";
 
-const Avatar: FC<{ alt: string; src: string; size?: string | number }> = (props) => {
-  return <img src={props.src} alt={props.alt} height={props.size} width={props.size} className={styles.root} />;
-};
+type ImageProps = ImgHTMLAttributes<HTMLImageElement>;
+
+interface IProps extends ImageProps {
+  size?: string | number;
+}
+
+const Avatar: FC<IProps> = (props) => (
+  <img
+    src={props.src}
+    alt={props.alt}
+    height={props.size}
+    width={props.size}
+    className={classNames(styles.root, props.className)}
+  />
+);
 
 export { Avatar };
